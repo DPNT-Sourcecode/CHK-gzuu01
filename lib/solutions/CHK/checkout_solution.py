c72f5e74c3aa3318e00ce423b74ac9a506ee741b
sbx_user1051 <sbx_user1051@169.254.9.21>
2022-03-12 15:54:25 +0000

    Sat Mar 12 15:54:25 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,32 @@
-
+from collections import defaultdict
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+def checkout(skus: list) -> int:
+
+    total = 0
+
+    goods = defaultdict(int)
+
+    for sku in skus:
+        goods[sku] += 1
+
+    for item, number in goods.items():
+        if item == "A":
+            special, nonspecial = divmod(number, 3)
+            total += special * 130
+            total += nonspecial * 50
+
+        elif item == "B":
+            special, nonspecial = divmod(number, 2)
+            total += special * 45
+            total += nonspecial * 30
+
+        elif item == "C":
+            total += number * 20
+
+        elif item == "D":
+            total += number * 15
+
+    return total
+
